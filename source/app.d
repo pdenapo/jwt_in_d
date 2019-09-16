a7446fe4dc190d862e1db4c8060a50a288745710
Pablo De Nápoli <pdenapo@gmail.com>
2019-09-16 09:23:43 -0300

    Added a minimal web interface. Just to tell the user that the server is running!

--- source/app.d
+++ source/app.d
@@ -7,34 +7,90 @@ import std.datetime.date;
 import std.datetime.systime;
 import std.json;
 import std.conv;
-import core.time : days;
+import core.time;
 
-const string  secret = "MiClaveDeTokens";
+import vibe.core.core;
+import vibe.core.log;
+import vibe.http.server;
+import vibe.http.router;
+import vibe.web.web;
+import vibe.web.rest;
+
+
+const string  secret = "My Token Generation Secret Key";
+
+const my_url = "127.0.0.1";
+
+const int my_port= 3000;
+
+
 
 struct User
 {
  string id;
 }
 
-string createToken(User who)
+interface APIRoot {
+ string getMessage();
+}
+
+
+class API : APIRoot
+{
+	
+	// Call it with http://127.0.0.1:3000/api/message 
+	
+	@safe override string getMessage()
+	{
+	  logInfo("get Message method called.");
+	  return "Jwt example in D";
+	}
+}
+
+
+string createToken(User who,  Duration expiration_time)
 {
   JSONValue[string] payload;
   payload["sub"] = JSONValue(who.id);
   SysTime currentTime = Clock.currTime();
   payload ["iat"] =  JSONValue(currentTime.toUnixTime());
   DateTime current_date = to!DateTime(currentTime);
-  DateTime expieraton_date =current_date + 10.days;
+  DateTime expieraton_date =current_date + expiration_time ;
   payload["exp"] = JSONValue(to!SysTime(expieraton_date).toUnixTime());
   JSONValue payload_json= JSONValue(payload);
-  writeln("Our payload in Json is:");
+  writeln("Our payload in Json is:\n");
   writeln(payload_json);
   return encode(payload_json,"secret",JWTAlgorithm.HS256);  	
+}
+
+
+class WebInterface
+{
+  
+  // GET /
+  void index()
+  {
+	render!("index.dt",my_url,my_port);
+  }
 }
 
 void main()
 {
  auto user = User("John");
- writeln("The generated web token is");
- writeln(createToken(user));
- writeln("You can test it using the debugger at https://jwt.io/#debugger-io");	
+ string token = createToken(user,10.days);
+ writeln("\nThe generated web token is \n");
+ writeln(token);
+ writeln("\nYou can test it using the debugger at https://jwt.io/#debugger-io");
+ 
+ 
+ auto settings = new HTTPServerSettings;
+ settings.port =  my_port;
+ settings.bindAddresses = [my_url];
+  
+ auto router = new URLRouter;
+ router.registerWebInterface(new WebInterface);
+ router.registerRestInterface(new API(),"/api");
+ 
+ listenHTTP(settings, router);
+ runApplication();	 	
 }
